c228b7542323ae78f7144ca7d051d3bc348bf41e
Tuna Güralp <57902543+TunaStark@users.noreply.github.com>
2022-09-14 18:57:47 +0300

    Add product style

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
     { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQRVSUQfIxY0b9eW8xCyIVC77rvk_SDBB-rv_RFTNrXSWmVD1kIIgterYycqq1YuTaVZDGtUi2PbPkyuUdXebgf3LOh1lHPyeu-AamrZmKa&usqp=CAE' },
@@ -9,8 +10,11 @@ const products = [
 ];
 
 const Products = () => {
+    const classes = useStyles();
+
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
