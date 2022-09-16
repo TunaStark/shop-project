1f3ad40db903446ce56b622ee0b2b94fa6bd80d1
Tuna Güralp <57902543+TunaStark@users.noreply.github.com>
2022-09-16 14:06:38 +0300

    Delete dummy data

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,12 +4,7 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQRVSUQfIxY0b9eW8xCyIVC77rvk_SDBB-rv_RFTNrXSWmVD1kIIgterYycqq1YuTaVZDGtUi2PbPkyuUdXebgf3LOh1lHPyeu-AamrZmKa&usqp=CAE' },
-    { id: 2, name: 'Macbook', description: 'Apple macbook.', price: '$10', image: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-space-gray-config-201810?wid=1078&hei=624&fmt=jpeg&qlt=90&.v=1633033424000' },
-];
-
-const Products = () => {
+const Products = ({ products }) => {
     const classes = useStyles();
 
     return (
